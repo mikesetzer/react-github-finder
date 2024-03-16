1a2d664fb8ce5beaf63a338fd59abe0877395e82
Mike Setzer <michaelsetzerucf@gmail.com>
2024-03-16 18:47:30 -0400

    ch16

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,12 @@
+
+import spinner from './spinner.gif';
+
+const Spinner = () => {
+  return (
+    <div>
+      <img src={spinner} alt="Loading..." style={{ width: '200px', margin: 'auto', display: 'block' }} />
+    </div>
+  )
+}
+
+export default Spinner
